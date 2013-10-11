81cbd8f4e8b803f0ddc4acb7e711a91fbc1895e6
atestthisis5 <atestthisis5@gmail.com>
2013-10-11 15:36:53 -0400

    clean up hello_world urls

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
-urlpatterns = patterns('',
-    url(r'^(P<name>.[^/]+)/$', 'hello_world.views.welcome', name='hello_world-welcome-name'),
-    url(r'^(P<name>.[^/]+)/(?P<age>\d+)/$', 'hello_world.views.welcome',
+urlpatterns = patterns('hello_world.views',
+    url(r'^(P<name>.[^/]+)/$', 'welcome', name='hello_world-welcome-name'),
+    url(r'^(P<name>.[^/]+)/(?P<age>\d+)/$', 'welcome',
         name='hello_world-welcome-name-age'),
+
 )
