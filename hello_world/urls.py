a7591e2259aa2686717a7691beead25bc4ee132b
atestthisis5 <atestthisis5@gmail.com>
2013-10-11 15:56:56 -0400

    fix urls ; add hello_world views

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('hello_world.views',
-    url(r'^(P<name>.[^/]+)/$', 'welcome', name='hello_world-welcome-name'),
-    url(r'^(P<name>.[^/]+)/(?P<age>\d+)/$', 'welcome',
+    url(r'^(?P<name>.[^/]+)/$', 'welcome', name='hello_world-welcome-name'),
+    url(r'^(?P<name>.[^/]+)/(?P<age>\d+)/$', 'welcome',
         name='hello_world-welcome-name-age'),
-
+    
 )
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,1 +1,24 @@
 # Create your views here.
+from django.http import HttpResponse, HttpResponseForbidden
+
+def welcome(request, name=None, age=None):
+    if name:
+        # The URL pattern matches only numbers, so we're not handling the 
+        # ValueError that might happen if age isn't a numeric value.
+        if not age or int(age) < 18:
+            return HttpResponseForbidden('Grow up!')
+        
+        response=HttpResponse()
+        
+        # Here we treat the response as a file-like object.
+        response.write('Hi %s, are you %s?' % (name, age, ))
+
+        # Here we just set a siller header (though a valid HTTP header
+        # would be just as applicable!)
+        response['X-python-header']='That parrot is definitely dead.'
+        return response
+    else:
+        # Here we pass it a string
+        return HttpResponse('Hello!')
+        # Here we pass it a generator (just as an example)
+        # return HttpResponse(('<br>%d % (i) for i in xrange(1,1000'))
