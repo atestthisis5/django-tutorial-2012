3420c10ff8a2679c1927b95c0f7606aa6dce11a7
atestthisis5 <atestthisis5@gmail.com>
2013-10-11 21:52:17 -0400

    add db setting and models

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Events(models.Model):
+    name = models.CharField(max_length=128,
+        help_text='Name of Event')
+    start_date = models.DateField(help_text='Start date of Event')
+    end_date = models.DateField(help_text='End date of Event')
+    start_time = models.TimeField(help_text='Start time of Event')
+    end_time = models.TimeField(help_text='End Time of Event')
+    description = models.TextField(help_text='Description of Event')
+
+class People(models.Model):
+    fname = models.CharField(max_length=64, help_text='First Name')
+    lname = models.CharField(max_length=64, help_text='Last Name')
+    email = models.EmailField(max_length=128, help_text='Email Address')
+    phone = models.CharField(max_length=16, help_text='Phone Number')
+    enrollments= models.ManyToManyField(Events)
