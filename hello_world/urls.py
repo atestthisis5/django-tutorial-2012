71b9800f3828803aba9fbaa742b0ac479510214d
atestthisis5 <atestthisis5@gmail.com>
2013-10-13 13:14:43 -0400

    add another template example

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('hello_world.views',
     url(r'^(?P<name>.[^/]+)/$', 'welcome', name='hello_world-welcome-name'),
     url(r'^(?P<name>.[^/]+)/(?P<age>\d+)/$', 'welcome',
         name='hello_world-welcome-name-age'),
+    url(r'^templateEx1$', 'template_ex1', name='hello_world-template_ex1'),
     url(r'^$', 'welcome', name='hello_world-welcome'),
 )
--- hello_world/views.py
+++ hello_world/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from datetime import datetime
 
 def welcome_v1(request, name=None, age=None):
     if name:
@@ -25,9 +26,30 @@ def welcome_v1(request, name=None, age=None):
         # Here we pass it a generator (just as an example)
         # return HttpResponse(('<br>%d' % (i) for i in xrange(1,1000)))
 
+
+def template_ex1(request):
+    context = {'month' : 'Jan',
+                'day' : 'Tuesday',
+                'people': ['JOE','Mark','Alan'],
+                'time' : datetime.today(),
+                'names' : {'Jones': 'Alan',
+                            'Anderson' : 'Jim',
+                            'Smith': 'Jonas'},
+                'autoescapedemo' : '''
+{% if (time.minute % 2 == 0) %}
+<p>The current minute {{ time.minute }} is even</p>
+{% else %}
+<p>The current minute {{ time.minute }} is odd</p>
+{% endif %}
+                ''',
+                }
+    return render_to_response('template_ex1.html', context)
+
+
 def welcome(request, name=None, age=None):
     context = {'age' : age,
             'name': name,
     }
 
     return render_to_response('welcome.html', context, context_instance=RequestContext(request))
+
