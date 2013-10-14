9739669040ba0d9449a1e2521de221453cf811d7
atestthisis5 <atestthisis5@gmail.com>
2013-10-13 22:32:15 -0400

    fix name of sqlite file

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django-tutorial-2012.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': 'tutorial.sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
